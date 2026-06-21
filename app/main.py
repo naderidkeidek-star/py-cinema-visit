8af13b1167ad7fc9636734e71e3314fe08675c24
Nader Idkeidek <nader.idkeidek@gmail.com>
2026-06-21 15:01:11 +0300

    Implement cinema_visit function for customer handling
    1st Act

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 # write your imports here
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    def cinema_visit(
+    customers: list[dict],
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    for customer in customers:
+        print(f"Cinema bar sold {customer['food']} to {customer['name']}.")
+
+    print(f'"{movie}" started in hall number {hall_number}.')
+
+    for customer in customers:
+        print(f"{customer['name']} is watching \"{movie}\".")
+
+    print(f'"{movie}" ended.')
+    print(f"Cleaner {cleaner} is cleaning hall number {hall_number}.")
