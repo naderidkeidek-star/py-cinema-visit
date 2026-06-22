0f4484e316841344cc3309e080c2f1751201c71b
Nader Idkeidek <nader.idkeidek@gmail.com>
2026-06-22 13:23:39 +0300

    Refactor cinema_visit function into CinemaBar class
    Removed the cinema_visit function and added CinemaBar class.

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,38 +1,4 @@
-#app/main.py
-from app.cinema.bar import CinemaBar
-from app.cinema.hall import CinemaHall
-from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
-
-
-def cinema_visit(
-    customers: list[dict],
-    hall_number: int,
-    cleaner: str,
-    movie: str
-) -> None:
-    customer_objects: list[Customer] = []
-
-    for customer in customers:
-        CinemaBar.sell_product(
-            customer["food"],
-            customer["name"]
-        )
-
-        customer_objects.append(
-            Customer(
-                customer["name"],
-                customer["food"]
-            )
-        )
-
-    hall = CinemaHall(hall_number)
-    cleaner_obj = Cleaner(cleaner)
-
-    hall.movie_session(
-        movie,
-        customer_objects,
-        cleaner_obj
-    )
-
-
+class CinemaBar:
+    @staticmethod
+    def sell_product(product: str, customer_name: str) -> None:
+        print(f"Cinema bar sold {product} to {customer_name}.")
