d2bdcb5a15b3cce04b086d47e59c4d97f74cb2c3
Nader Idkeidek <nader.idkeidek@gmail.com>
2026-06-22 12:33:15 +0300

    Implement cinema_visit function with customer handling
    3rd Refactor cinema_visit function to use Customer objects and integrate CinemaBar and CinemaHall.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list[dict],
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    customer_objects: list[Customer] = []
+
+    for customer in customers:
+        CinemaBar.sell_product(
+            customer["food"],
+            customer["name"]
+        )
+
+        customer_objects.append(
+            Customer(
+                customer["name"],
+                customer["food"]
+            )
+        )
+
+    hall = CinemaHall(hall_number)
+    cleaner_obj = Cleaner(cleaner)
+
+    hall.movie_session(
+        movie,
+        customer_objects,
+        cleaner_obj
+    )
